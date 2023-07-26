93ae15cfa193177c6074084de7d159c73b56f1c8
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2023-07-26 22:02:19 +0200

    Ex 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,9 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
