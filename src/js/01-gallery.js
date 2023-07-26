c7c6c25cb70e93d32774281e95a2169593927783
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2023-07-27 00:29:02 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -9,3 +9,28 @@ console.log(galleryItems);
 import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const galleryList = document.querySelector('.gallery');
+
+function renderGallery() {
+  const createElement = galleryItems
+    .map(
+      item =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+    </li>`
+    )
+    .join('');
+
+  galleryList.innerHTML = createElement;
+}
+
+renderGallery();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: '250',
+  captionsData: 'alt',
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,10 @@
+const form = document.querySelector('.feedback-form');
 
+console.log(form.event.target);
+function saveData(event) {
+    const { email, message } = event.taget;
+
+    localStorage.setItem(email, message);
+}
+
+form.addEventListener('input', saveData())
