96613438fb02025d648321e4761f6e5de26df640
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2023-07-26 22:29:33 +0000

    Deploying to gh-pages from @ NickPosvystak/goit-js-hw-08@c7c6c25cb70e93d32774281e95a2169593927783 🚀

---
+++ 03-feedback.2d2f4fd5.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".feedback-form");console.log(e.event.target),e.addEventListener("input",function(e){const{email:t,message:o}=e.taget;localStorage.setItem(t,o)}());
+//# sourceMappingURL=03-feedback.2d2f4fd5.js.map
