dd260d3917c4755cf08ea858b090a87fb27892f5
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2023-07-31 18:07:20 +0200

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,37 +1,15 @@
-
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const Vimeo = require('@vimeo/player');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-// const iframe = document.getElementById('vimeo-player');
+player.on(
+  'timeupdate', throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
+  }),
+  1000
+);
 
-
-// const Vimeo = require('vimeo-player');
-// const throttle = require('lodash.throttle');
-
-// const iframe = document.getElementById('vimeo-player');
-
-// // Ініціалізуємо плеєр для відео в iframe
-// const player = new Vimeo.Player(iframe);
-
-// // Функція для збереження часу відтворення у локальне сховище
-// const saveCurrentTimeToStorage = throttle(function (currentTime) {
-//   localStorage.setItem('videoplayer-current-time', currentTime.toString());
-// }, 1000); // Збереження не частіше, ніж раз на секунду
-
-// // Підключаємо обробник події timeupdate для відстеження часу відтворення
-// player.on('timeupdate', function (data) {
-//   const currentTime = data.seconds; // Поточний час відтворення у секундах
-//   // Збережемо час відтворення у локальне сховище
-//   saveCurrentTimeToStorage(currentTime);
-// });
-
-// // Зчитуємо час відтворення з локального сховища після перезавантаження сторінки
-// window.onload = function () {
-//   const savedTime = localStorage.getItem('videoplayer-current-time');
-
-//   if (savedTime) {
-//     // Встановлюємо час відтворення, якщо він був збережений
-//     player.setCurrentTime(parseFloat(savedTime));
-//   }
-// };
+const currentTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(currentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,37 @@
+
+import throttle from 'lodash.throttle';
+
 const form = document.querySelector('.feedback-form');
 
-console.log(form.event.target);
-function saveData(event) {
-    const { email, message } = event.taget;
+let emailValue = '';
+let messageValue = '';
 
-    localStorage.setItem(email, message);
+const data = {
+    email: emailValue,
+    message: messageValue,
 }
+const savedData = JSON.parse(localStorage.getItem("feedback-form-state"));
+if(savedData){
+    form.elements.email.value = savedData.email;
+    data.elements.message.value = savedData.message;
 
-form.addEventListener('input', saveData())
+
+}
+form.addEventListener('input', throttle(onInput, 500));
+
+function onInput(){
+    data.email = form.elements.email.value;
+    data.message = form.elements.message.value;
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(data));
+}
+form.addEventListener('submit', onSubmit)
+
+function onSubmit(event) {
+    event.preventDefault();
+    
+    console.log(data);
+    
+    localStorage.removeItem("feedback-form-state");
+    form.reset(form);
+}
